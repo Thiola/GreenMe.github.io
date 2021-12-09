9a5ffd87c3ad5040dbd537e0dccc7e5b0757d306
Thiola <93956307+Thiola@users.noreply.github.com>
2021-12-09 23:43:10 +0700

    Add files via upload

---
+++ komen.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>pasang komentar</title>
+
+	<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v12.0" nonce="lJzHoi2b"></script>
+</head>
+
+
+<body>
+	<h1>cara pasang komen</h1>
+	<p>
+		lorem hscbjnvm
+		lkjdjf
+		fjdhf
+		fhhfj
+		fhwfh
+
+	</p>
+
+	<html
+				$uri= $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+				echo $uri; >
+
+	?>
+	<div class="fb-comments" data-href="http://localhost/world/komen.php" data-width="" data-numposts="5"></div>
+
+</body>
